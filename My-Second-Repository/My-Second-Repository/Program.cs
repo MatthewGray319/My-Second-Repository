86cfb0b9a230539808add525055875e9af4a7694
Matthew Gray <matthewcharlesgray319@gmail.com>
2022-09-08 17:54:45 -0400

    My second repository made

---
+++ My-Second-Repository/My-Second-Repository/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var isSunny = true;
+var isRaining = false;
+if (isSunny) 
+{
+    Console.WriteLine("It is sunny!");
+}
